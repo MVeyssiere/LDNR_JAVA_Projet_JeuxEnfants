02552dde99d41e343eb1d2840ba2f977eec43c04
stagldnr <stagldnr@debian-java-ldnr.ldnr.local>
2019-03-25 09:54:30 +0100

    test2

--- JeuxEnfant/src/jeuxenfant/JeuxEnfant.java
+++ JeuxEnfant/src/jeuxenfant/JeuxEnfant.java
@@ -16,7 +16,7 @@ public class JeuxEnfant {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("test");
+        System.out.println("test dcqsc");
     }
     
 }
