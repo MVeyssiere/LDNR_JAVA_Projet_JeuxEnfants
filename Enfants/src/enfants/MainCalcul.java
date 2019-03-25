b75342c828aefde04e285d7808a182a0ff1071af
stagldnr <stagldnr@debian-java-ldnr.ldnr.local>
2019-03-25 11:23:36 +0100

    Premier affichage opérations

--- Enfants/src/enfants/MainCalcul.java
+++ Enfants/src/enfants/MainCalcul.java
@@ -5,6 +5,7 @@
  */
 package enfants;
 
+import java.awt.FlowLayout;
 import java.awt.Insets;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -22,30 +23,47 @@ public class MainCalcul {
         JFrame.setDefaultLookAndFeelDecorated(true);
         JFrame frame = new JFrame("CALCUL");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panelGeneral = new JPanel();
+        BoxLayout boxGeneral = new BoxLayout(panelGeneral, BoxLayout.PAGE_AXIS);
+        panelGeneral.setLayout(boxGeneral);
 
-        JPanel panel = new JPanel();
-
-        BoxLayout boxlayout = new BoxLayout(panel, BoxLayout.Y_AXIS);
-
-        panel.setLayout(boxlayout);
-
-        panel.setBorder(new EmptyBorder(new Insets(150, 200, 150, 200)));
+        JPanel operation = new JPanel(new FlowLayout());
+//        BoxLayout boxlayout = new BoxLayout(operation, BoxLayout.LINE_AXIS);
+//        operation.setLayout(boxlayout);
+//        operation.setBorder(new EmptyBorder(new Insets(150, 200, 150, 200)));
 
         JButton jb1 = new JButton("0-9");
         JButton jb2 = new JButton("0-9");
         JButton jb3 = new JButton("+ -");
-        JButton jb4 = new JButton("Verifier");
-        JButton jb5 = new JButton("Solution");
-        JButton jb6 = new JButton("Autre calcul");
+        
+        operation.add(jb1);
+        operation.add(jb2);
+        operation.add(jb3);
+        
+       
+        
+        JPanel choix = new JPanel();
+        BoxLayout boxlayout2 = new BoxLayout(choix, BoxLayout.Y_AXIS);
+        choix.setLayout(boxlayout2);
+        choix.setBorder(new EmptyBorder(new Insets(150, 200, 150, 200)));
 
-        panel.add(jb1);
-        panel.add(jb2);
-        panel.add(jb3);
-        panel.add(jb4);
-        panel.add(jb5);
-        panel.add(jb6);
+      
+        JButton jb4 = new JButton("Résultat");
+        JButton jb5 = new JButton("Verifier");
+        JButton jb6 = new JButton("Solution");
+        JButton jb7 = new JButton("Autre calcul");
 
-        frame.add(panel);
+      
+        choix.add(jb4);
+        choix.add(jb5);
+        choix.add(jb6);
+        choix.add(jb7);
+
+        panelGeneral.add(operation);
+        panelGeneral.add(choix);
+        frame.add(panelGeneral);
+//        frame.add(operation);
+//        frame.add(choix);
         frame.pack();
         frame.setVisible(true);
 
